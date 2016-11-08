54b396dbc6ab79b9279b747f163a27afd1b2a0b5
Tarun <tarunkumar1391@outlook.com>
2016-11-08 15:43:33 +0100

    few UI changes to dashboard page

--- server/fetchalloted.php
+++ server/fetchalloted.php
@@ -19,6 +19,7 @@ while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     $outp .= '"selectedSoft":"'  . $rs["selectedSoft"] . '",';
     $outp .= '"nokl":"'  . $rs["nokl"] . '",';
     $outp .= '"nameofRequester":"'   . $rs["nameofRequester"] . '",';
+    $outp .= '"email":"'   . $rs["requester_email"] . '",';
     $outp .= '"mac_device":"'   . $rs["mac_device"] . '",';
     $outp .= '"requester_roomNo":"'   . $rs["requester_roomNo"] . '",';
     $outp .= '"requester_groupName":"'   . $rs["requester_groupName"] . '",';
